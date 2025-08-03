affe4e6cc2e52542c954f9d6e05b68875337c560
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-08-03 22:23:13 +0100

    updated script

--- install-tmux.sh
+++ install-tmux.sh
@@ -3,4 +3,8 @@
 sudo dnf install --refresh -y tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ./.tmux.conf ~/.tmux.conf
+# launch tmux server to reload conf
+tmux new -d
 tmux source ~/.tmux.conf
+tmux kill-server
+echo "Config complete! Launch tmux and press 'prefix (Ctrl+s) + I' to install plugins"
