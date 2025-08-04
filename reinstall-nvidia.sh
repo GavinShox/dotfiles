8f843fa6fe68f533c8b534286ee26eef657659ec
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-08-04 16:17:36 +0100

    added reinstall nvidia drivers util script for fedora

---
+++ reinstall-nvidia.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "Re-installing Nvidia drivers"
+dnf remove -y \*nvidia\* --exclude nvidia-gpu-firmware
+dnf install -y akmod-nvidia
+
+echo "Wait for kernel module to compile before restarting - 'modinfo -f version nvidia' should return the module version after it is compiled"
