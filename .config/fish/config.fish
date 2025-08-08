d967c720724e29f46b693073a9920ee1f6b1cd22
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-08-08 23:03:20 +0100

    moved abbreviations to main fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ bind \cf zi # bind control + f to zi (zoxide)
 set -gx EDITOR micro
 set -gx MICRO_TRUECOLOR 1
 
+abbr -a sedit sudo $EDITOR
+
 starship init fish | source
 fzf --fish | source
 zoxide init fish | source
