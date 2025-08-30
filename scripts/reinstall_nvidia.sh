1e526935b8bc540e79669a1ceead320165cc693d
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-08-31 00:23:42 +0100

    reinstall nvidia script now reinstalls nvidia-smi package

--- scripts/reinstall_nvidia.sh
+++ scripts/reinstall_nvidia.sh
@@ -7,6 +7,6 @@ fi
 
 echo "Re-installing Nvidia drivers"
 dnf remove -y \*nvidia\* --exclude nvidia-gpu-firmware
-dnf install -y akmod-nvidia
+dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 
 echo "Wait for kernel module to compile before restarting - 'modinfo -f version nvidia' should return the module version after it is compiled"
