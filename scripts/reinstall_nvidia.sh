1d79ca61d4dbc809f4b5e3d84db380b7d8bf0690
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-09-05 00:02:00 +0100

    nvidia reinstall now asks for confirmation, and handles errors

--- scripts/reinstall_nvidia.sh
+++ scripts/reinstall_nvidia.sh
@@ -1,15 +1,31 @@
 #! /bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+echo "-------------------------------- Reinstall Nvidia Drivers --------------------------------"
+
+read -r -p "This script will reinstall your Nvidia drivers. It requires sudo privileges - Continue? (y/n): " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+	echo "Stopping script..."
+	exit 1
 fi
 
-echo "-------------------------------- Reinstall Nvidia Drivers --------------------------------"
 echo "Removing all nvidia drivers, excluding nvidia-gpu-firmware"
-dnf remove -y \*nvidia\* --exclude nvidia-gpu-firmware
-echo "Reinstalling nvidia drivers and nvidia-smi"
-dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+echo "Trying to execute: 'sudo dnf remove -y \*nvidia\* --exclude nvidia-gpu-firmware'"
 
-echo "Wait for kernel module to compile before restarting - 'modinfo -f version nvidia' should return the module version after it is compiled"
+sudo dnf remove -y \*nvidia\* --exclude nvidia-gpu-firmware || {
+	echo "Error: Couldn't execute command with sudo. Exiting...";
+	echo "---------------------------------------------------------------------------------------------";
+	exit 1;
+}
+
+echo "Reinstalling nvidia drivers and nvidia-smi"
+echo "Trying to execute: 'sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda'"
+sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda || {
+	echo "Error: Couldn't execute command with sudo. Please run the given reinstall command to get the nvidia drivers. Exiting...";
+	echo "---------------------------------------------------------------------------------------------";
+	exit 1;
+}
+
+echo "Done!"
+echo "Wait for kernel module to compile before restarting - 'modinfo -f version nvidia' should return the module version after it is compiled, or watch for an 'akmods' process to complete"
 echo "-------------------------------- Nvidia Drivers Reinstalled! --------------------------------"
+
