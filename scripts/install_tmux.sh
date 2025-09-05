3d929bd41862d9d15e57e17c3766e7f099423bbf
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-09-05 01:05:27 +0100

    added confirmation for install_tmux script

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -2,6 +2,14 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "-------------------------------- Install & Setup Tmux --------------------------------"
+
+read -r -p "This script will install tmux and plugin manager from repo: <https://github.com/tmux-plugins/tpm> - Continue? (y/n): " input
+if [[ ! $input =~ ^[Yy]$ ]]; then
+	echo "Stopping script..."
+	"--------------------------------------------------------------------------------------"
+	exit 1
+fi
+
 sudo dnf install --refresh -y tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp -fb $SCRIPT_DIR/../configs/.tmux.conf $HOME/.tmux.conf
