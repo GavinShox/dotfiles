033e4ffe2fd193e232ff981c1bd74247bdb9a652
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-09-08 15:38:47 +0100

    --all on diskspace

--- configs/.config/fish/functions/diskspace.fish
+++ configs/.config/fish/functions/diskspace.fish
@@ -1,4 +1,4 @@
 function diskspace --wraps='du -d1 -h | sort -h -r' --description 'alias diskspace=du -d1 -h | sort -h -r'
-  du -d1 -h $argv | sort -h -r
+  du -d1 -h --all $argv | sort -h -r
         
 end
