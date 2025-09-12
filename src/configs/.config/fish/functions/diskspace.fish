349273757e8e8ff92d31be7b722c6796e8d8d62a
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-09-12 22:07:13 +0100

    diskspace doesnt reverse sort

--- src/configs/.config/fish/functions/diskspace.fish
+++ src/configs/.config/fish/functions/diskspace.fish
@@ -1,4 +1,4 @@
-function diskspace --wraps='du -d1 -h | sort -h -r' --description 'alias diskspace=du -d1 -h | sort -h -r'
-  du -d1 -h --all $argv | sort -h -r
+function diskspace --wraps='du -d1 -h | sort -h' --description 'alias diskspace=du -d1 -h | sort -h'
+  du -d1 -h --all $argv | sort -h
         
 end
