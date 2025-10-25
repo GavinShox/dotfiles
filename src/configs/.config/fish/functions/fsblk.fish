fcb5dc5d5f6758bdc35a4d67fdc410cb9ebf93b5
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2025-10-25 19:25:45 +0000

    updated function

--- src/configs/.config/fish/functions/fsblk.fish
+++ src/configs/.config/fish/functions/fsblk.fish
@@ -1,4 +1,4 @@
-function fsblk --wraps='lsblk -o NAME,RM,SIZE,FSAVAIL,FSUSE%,RO,TYPE,MOUNTPOINTS' --description 'alias fsblk=lsblk -o NAME,RM,SIZE,FSAVAIL,FSUSE%,RO,TYPE,MOUNTPOINTS'
-  lsblk -o NAME,RM,SIZE,FSAVAIL,FSUSE%,RO,TYPE,FSTYPE,MOUNTPOINTS $argv
+function fsblk --wraps='lsblk -o NAME,RM,SIZE,FSAVAIL,FSUSE%,RO,TYPE,MOUNTPOINTS -e 7' --description 'alias fsblk=lsblk -o NAME,RM,SIZE,FSAVAIL,FSUSE%,RO,TYPE,MOUNTPOINTS -e 7'
+  lsblk -o NAME,RM,SIZE,FSAVAIL,FSUSE%,RO,TYPE,FSTYPE,MOUNTPOINTS -e 7 $argv
         
 end
